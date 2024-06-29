28c8c354fc135832b6d614553142e8720bc230d3
PriyanshuArya678 <105437949+PriyanshuArya678@users.noreply.github.com>
2024-06-29 23:10:21 +0530

    x

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 import {connectDb} from './connectDb.js'
+import userModel from './models/userSchema.js'
 const app=express()
+app.use(cors({
+    origin:'http://localhost:5173'
+}))
+app.post('/login',(req,res)=>{
+    userModel.create({})
+})
 app.listen (3000,async()=>{
     connectDb()
     console.log("on port 3000")
