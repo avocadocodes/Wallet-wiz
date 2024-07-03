a7092d6932fa70565e57de22fe3cee4a72287ef9
PriyanshuArya678 <105437949+PriyanshuArya678@users.noreply.github.com>
2024-07-03 23:13:22 +0530

    x

--- Wallet-wiz/src/components/Landingpage.jsx
+++ Wallet-wiz/src/components/Landingpage.jsx
@@ -2,19 +2,21 @@ import React, { useEffect, useState } from 'react';
 import SideBar from './SideBar';
 import Modal from './Modal';
 import { useNavigate } from 'react-router-dom';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import Profile from './Profile';
 import Transaction from './Transaction';
 import { sendMoney } from '../Store/userDataSlice';
 import TopBar from './TopBar';
+
 export function Landingpage() {
   const [transactions, setTransactions] = useState([]);
   const [showRequestModal, setShowRequestModal] = useState(false);
   const [showSendModal, setShowSendModal] = useState(false);
   const userStatus = useSelector((state) => state.userStatus);
   const navigate = useNavigate();
-  const dispatch =useDispatch()
+  const dispatch = useDispatch();
+
   const handleShowRequestModal = () => {
     setShowRequestModal(true);
   };
@@ -36,37 +38,36 @@ export function Landingpage() {
       const res = await axios.post('http://localhost:3000/requestMoney', {
         senderEmail: userStatus.userStatus.email,
         receiverEmail: email,
-        amount:  parseInt(amount, 10)
+        amount: parseInt(amount, 10)
       });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const handleSendSubmit = async (formData) => {
-    try {
-      const email = formData.get('email');
-      const amount = formData.get('amount');
-      if(amount>userStatus.userStatus.balance){
-        alert("you don't have enough balane")
-        return 
-      }
-      console.log(userStatus.userStatus.email);
-      const res = await axios.post('http://localhost:3000/sendMoney', {
-        senderEmail: userStatus.userStatus.email,
-        receiverEmail: email,
-        amount:  parseInt(amount, 10)
-      });
-      console.log(res);
-      if(res.status==200){
-        const {moneyReceived,moneySent,balance}=res.data
-        await dispatch(sendMoney({balance:balance,moneyReceived:moneyReceived,moneySent:moneySent}))
-        
-      }
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleSendSubmit = async (formData) => {
+    try {
+      const email = formData.get('email');
+      const amount = formData.get('amount');
+      if (amount > userStatus.userStatus.balance) {
+        alert("You don't have enough balance");
+        return;
+      }
+      console.log(userStatus.userStatus.email);
+      const res = await axios.post('http://localhost:3000/sendMoney', {
+        senderEmail: userStatus.userStatus.email,
+        receiverEmail: email,
+        amount: parseInt(amount, 10)
+      });
+      console.log(res);
+      if (res.status === 200) {
+        const { moneyReceived, moneySent, balance } = res.data;
+        await dispatch(sendMoney({ balance: balance, moneyReceived: moneyReceived, moneySent: moneySent }));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     if (!userStatus.userStatus.loggedIn) {
@@ -78,10 +79,10 @@ export function Landingpage() {
   return (
     <div className="flex flex-row justify-between w-full p-5 h-screen">
       <SideBar onRequest={handleShowRequestModal} onSend={handleShowSendModal} />
-      <div className='flex flex-col w-full  mx-10'>
-        <Profile/>
-        <TopBar/>
-        <Transaction/>
+      <div className='flex flex-col w-full mx-10'>
+        <Profile />
+        <TopBar />
+        <Transaction />
       </div>
       <Modal show={showRequestModal} onClose={handleCloseModal} modalContent="Request Money" onSubmit={handleRequestSubmit} />
       <Modal show={showSendModal} onClose={handleCloseModal} modalContent="Send Money" onSubmit={handleSendSubmit} />
